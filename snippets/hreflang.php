3159a220ad2fcad2fe3f84df0192d3d9c44fafc9
Philipp Trenz <mail@philipptrenz.de>
2022-08-09 09:46:05 +0200

    Add canonical link for pages that have not been translated

--- snippets/hreflang.php
+++ snippets/hreflang.php
@@ -2,3 +2,12 @@
 <link rel="alternate" hreflang="<?php echo strtolower(html($language->code())); ?>" href="<?= $page->url($language->code()); ?>" />
 <?php endforeach; ?>
 <link rel="alternate" hreflang="x-default" href="<?= $page->url($kirby->defaultLanguage()->code()); ?>" />
+<?php 
+    // Add canonical link for pages that have not been translated
+    $isTranslated = $page->translation($language->code())->exists();
+    if ($language->isDefault() === false && $isTranslated === false): 
+?>
+<link rel="canonical" link="<?= $isTranslated ? $page->url(): $page->url($kirby->defaultLanguage()->code())?>"/>
+<?php 
+    endif 
+?>
